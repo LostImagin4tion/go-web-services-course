45dba846899b4ee0698c62fd21ef8dbc1c67b271
danilov6083 <danilov6083@outlook.com>
2025-07-16 01:23:25 +0300

    [ARCHITECTURE] added utils and entities

--- 9-architecture/main.go
+++ 9-architecture/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"stepikGoWebServices/handlers"
 )
 
 func main() {
 	addr := ":8080"
-	h := GetApp()
+	h := handlers.NewRealWorldApi()
 	fmt.Println("start server at", addr)
 	http.ListenAndServe(addr, h)
 }
--- 9-architecture/realworld.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"net/http"
-)
-
-func GetApp() http.Handler {
-	return nil
-}
